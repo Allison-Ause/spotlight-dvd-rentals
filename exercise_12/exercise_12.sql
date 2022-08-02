4ab2ee8afdde310e840a07abd93b470a1bd2a6f3
Allison Ause <allison.ause@gmail.com>
2022-08-02 14:38:24 -0700

    passing test for exercise 13

--- exercise_12/exercise_12.sql
+++ exercise_12/exercise_12.sql
@@ -1,3 +1,14 @@
 -- the top 10 cities with the most total rental payment amount
 -- include the city name and sum
 -- use SUM to sum up the amounts
+
+-- city, address, customer, rental, payment
+
+SELECT city.city, SUM(payment.amount) FROM city
+INNER JOIN address ON city.city_id = address.city_id
+INNER JOIN customer ON address.address_id = customer.address_id
+INNER JOIN rental ON customer.customer_id = rental.customer_id
+INNER JOIN payment ON rental.rental_id = payment.rental_id
+GROUP BY city.city_id
+ORDER BY sum DESC
+LIMIT 10
--- exercise_13/exercise_13.sql
+++ exercise_13/exercise_13.sql
@@ -1,3 +1,15 @@
 -- the average rental amount by country
 -- include the country name and avg amount
 -- use AVG to average the amount
+
+-- country, city, address, customer, rental, payment
+
+SELECT country.country, AVG(payment.amount) FROM country
+INNER JOIN city ON country.country_id = city.country_id
+INNER JOIN address ON city.city_id = address.city_id
+INNER JOIN customer ON address.address_id = customer.address_id
+INNER JOIN rental ON customer.customer_id = rental.customer_id
+INNER JOIN payment ON rental.rental_id = payment.rental_id
+GROUP BY country.country
+ORDER BY AVG DESC
+LIMIT 10
